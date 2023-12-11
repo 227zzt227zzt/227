9414b446c5639e9558514079e7886ad35fa278b4
227zzt227zzt <124112749+227zzt227zzt@users.noreply.github.com>
2023-12-11 10:45:44 +0800

    Add files via upload

---
+++ ks/data.js
@@ -0,0 +1,29 @@
+var mediaData = {
+  audioPath: "d:/media/mp3Files/",
+  audioFiles: [
+    {
+      fileName: "LinuxCMD-0.mp3",
+      titles: ["preface", "thanks", "history", "preface", "thanks", "history"],
+      time: [
+        [5, 200],
+        [203, 500],
+        [503, 900],
+      ],
+    },
+    ,
+    "LinuxCMD-1.mp3",
+    "LinuxCMD-2.mp3",
+  ],
+  videoPath: "d:/media/mp4Files/",
+  videoFiles: [
+    "v02004a40000bproefir863oaub47slg",
+    "v02004a40000bproicl7gl1h25fgjoeg",
+    "v02004a50000bpmepbubn5v2f7sn39og",
+    "v02004a80000bn2mnkonrm1ii1qe45bg",
+    "v02004ac0000bt20l8lo827ubql9vlt0",
+    "v02004b40000bnac1ktjfrmsnk5rbis0",
+    "v02004b40000bo039k481ukr122vand0",
+    "v02004b70000bqud654qs4skvnoev5k0",
+    "v0201ag10000caikc13c77udap1dkdb0",
+  ],
+};
